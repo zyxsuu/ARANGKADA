e6b3b3c573635d1ad14a4ea47c8a2b754ff91323
zyxsuu <you@example.com>
2026-05-20 05:20:14 +0800

    added startShift and endShift() on MainActivity.java

--- app/src/main/java/com/example/arangkada/MainActivity.java
+++ app/src/main/java/com/example/arangkada/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.arangkada;
 
+import android.Manifest;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,8 +16,9 @@ import com.example.arangkada.services.TrackingService;
 public class MainActivity extends AppCompatActivity {
 
     DatabaseHelper db;
-
     BroadcastReceiver receiver;
+
+    private double liveDistance = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,24 +27,29 @@ public class MainActivity extends AppCompatActivity {
 
         db = new DatabaseHelper(this);
 
-        db.insertShift(
-                "May 18",
-                "8:00 AM",
-                "4:00 PM",
-                42.7,
-                650,
-                180,
-                1.4,
-                470,
-                30.5
-        );
+        if (checkSelfPermission(
+                Manifest.permission.ACCESS_FINE_LOCATION
+        ) != PackageManager.PERMISSION_GRANTED) {
+
+            requestPermissions(
+                    new String[]{
+                            Manifest.permission.ACCESS_FINE_LOCATION
+                    },
+                    1
+            );
+        }
 
         receiver = new BroadcastReceiver() {
             @Override
             public void onReceive(Context context, Intent intent) {
-                double distance = intent.getDoubleExtra("distance", 0);
 
-                // update UI here (TextView later)
+                liveDistance =
+                        intent.getDoubleExtra(
+                                "distance",
+                                0
+                        );
+
+                // update TextView later
             }
         };
 
@@ -51,11 +59,45 @@ public class MainActivity extends AppCompatActivity {
                 Context.RECEIVER_NOT_EXPORTED
         );
 
-        // START GPS SERVICE
-        Intent startIntent = new Intent(this, TrackingService.class);
-        startService(startIntent);
+        // TEMPORARY TESTING
+        startShift();
 
-        // STOP  THE GPS SERVICE
+        // TEST END SHIFT AFTER A FEW SECONDS MANUALLY
+        // endShift();
+    }
+
+    private void startShift() {
+
+        TrackingService.currentDistance = 0;
+
+        Intent startIntent =
+                new Intent(
+                        this,
+                        TrackingService.class
+                );
+
+        startService(startIntent);
+    }
+
+    private void endShift() {
+
+        Intent stopIntent =
+                new Intent(
+                        this,
+                        TrackingService.class
+                );
+
+        stopService(stopIntent);
+
+        double finalDistance =
+                TrackingService.currentDistance;
+
+        db.saveShiftMinimal(
+                "May 20",
+                "8:00 AM",
+                "4:00 PM",
+                finalDistance
+        );
     }
 
     @Override
--- app/src/main/java/com/example/arangkada/services/TrackingService.java
+++ app/src/main/java/com/example/arangkada/services/TrackingService.java
@@ -1,18 +1,17 @@
 package com.example.arangkada.services;
 
-
 import android.Manifest;
-import android.content.pm.PackageManager;
-import androidx.core.app.ActivityCompat;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.Service;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.IBinder;
 
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 import androidx.core.app.NotificationCompat;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
@@ -23,7 +22,10 @@ import com.google.android.gms.location.LocationServices;
 
 public class TrackingService extends Service {
 
-    private static final String CHANNEL_ID = "ArangkadaTracking";
+    private static final String CHANNEL_ID =
+            "ArangkadaTracking";
+
+    public static double currentDistance = 0.0;
 
     private FusedLocationProviderClient fusedLocationClient;
     private LocationCallback locationCallback;
@@ -35,35 +37,72 @@ public class TrackingService extends Service {
     public void onCreate() {
         super.onCreate();
 
-        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        fusedLocationClient =
+                LocationServices
+                        .getFusedLocationProviderClient(this);
 
         createNotificationChannel();
-        startForeground(1, createNotification("Shift Active", "Tracking distance..."));
 
-        LocationRequest request = LocationRequest.create();
+        startForeground(
+                1,
+                createNotification(
+                        "Shift Active",
+                        "Tracking distance..."
+                )
+        );
+
+        LocationRequest request =
+                LocationRequest.create();
+
         request.setInterval(3000);
         request.setFastestInterval(2000);
-        request.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        request.setPriority(
+                LocationRequest.PRIORITY_HIGH_ACCURACY
+        );
 
-        locationCallback = new LocationCallback() {
-            @Override
-            public void onLocationResult(LocationResult locationResult) {
-                if (locationResult == null) return;
+        locationCallback =
+                new LocationCallback() {
 
-                Location location = locationResult.getLastLocation();
+                    @Override
+                    public void onLocationResult(
+                            LocationResult locationResult
+                    ) {
 
-                if (lastLocation != null) {
-                    float distance = lastLocation.distanceTo(location);
-                    totalDistance += distance;
-                }
+                        if (locationResult == null)
+                            return;
 
-                lastLocation = location;
+                        Location location =
+                                locationResult
+                                        .getLastLocation();
 
-                Intent intent = new Intent("LOCATION_UPDATE");
-                intent.putExtra("distance", totalDistance / 1000);
-                sendBroadcast(intent);
-            }
-        };
+                        if (lastLocation != null) {
+
+                            float distance =
+                                    lastLocation.distanceTo(
+                                            location
+                                    );
+
+                            totalDistance += distance;
+
+                            currentDistance =
+                                    totalDistance / 1000;
+                        }
+
+                        lastLocation = location;
+
+                        Intent intent =
+                                new Intent(
+                                        "LOCATION_UPDATE"
+                                );
+
+                        intent.putExtra(
+                                "distance",
+                                currentDistance
+                        );
+
+                        sendBroadcast(intent);
+                    }
+                };
 
         if (ActivityCompat.checkSelfPermission(
                 this,
@@ -86,14 +125,25 @@ public class TrackingService extends Service {
     }
 
     @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
+    public int onStartCommand(
+            Intent intent,
+            int flags,
+            int startId
+    ) {
         return START_STICKY;
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
-        fusedLocationClient.removeLocationUpdates(locationCallback);
+
+        if (locationCallback != null) {
+
+            fusedLocationClient
+                    .removeLocationUpdates(
+                            locationCallback
+                    );
+        }
     }
 
     @Nullable
@@ -102,22 +152,41 @@ public class TrackingService extends Service {
         return null;
     }
 
-    private Notification createNotification(String title, String text) {
-        return new NotificationCompat.Builder(this, CHANNEL_ID)
+    private Notification createNotification(
+            String title,
+            String text
+    ) {
+
+        return new NotificationCompat.Builder(
+                this,
+                CHANNEL_ID
+        )
                 .setContentTitle(title)
                 .setContentText(text)
-                .setSmallIcon(android.R.drawable.ic_menu_mylocation)
+                .setSmallIcon(
+                        android.R.drawable
+                                .ic_menu_mylocation
+                )
                 .build();
     }
 
     private void createNotificationChannel() {
-        NotificationChannel channel = new NotificationChannel(
-                CHANNEL_ID,
-                "Arangkada Tracking",
-                NotificationManager.IMPORTANCE_LOW
-        );
 
-        NotificationManager manager = getSystemService(NotificationManager.class);
-        manager.createNotificationChannel(channel);
+        NotificationChannel channel =
+                new NotificationChannel(
+                        CHANNEL_ID,
+                        "Arangkada Tracking",
+                        NotificationManager
+                                .IMPORTANCE_LOW
+                );
+
+        NotificationManager manager =
+                getSystemService(
+                        NotificationManager.class
+                );
+
+        manager.createNotificationChannel(
+                channel
+        );
     }
 }
